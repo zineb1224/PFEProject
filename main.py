af0fb47978004845e2bdc07ec76760f74c63d7f6
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-04-30 17:21:33 +0100

    spam model

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from sklearn import datasets
-cancer = datasets.load_breast_cancer()
-#print(cancer.data[:5])
-#print(cancer.target)
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-x_train , x_test , y_train , y_test = train_test_split(cancer.data , cancer.target , test_size = 0.50 , random_state = 109)
-from sklearn import svm
-clf = svm.SVC(kernel = 'linear')
-clf.fit(x_train , y_train)
-y_pred = clf.predict(x_test)
-from sklearn import metrics
-print("accuracy :" , metrics.accuracy_score(y_test , y_pred))
+from tkinter import *
+from models.SVMModelSpam import SVMModelSpam, import_data
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
+
+svmmodelSpam = SVMModelSpam()
+
+# Chargement des données
+emails_data = import_data('datasets/labeled_emails.csv')
+
+# Séparation des données en ensembles d'entraînement et de test
+emails = emails_data['email']
+labels = np.where(emails_data['label'] == 'spam', 1, 0)  # Encoder les étiquettes en 0 et 1
+
+# Diviser les données en ensembles d'entraînement et de test
+emails_train, emails_test, labels_train, labels_test = train_test_split(emails, labels, test_size=0.2, random_state=42)
+
+# Vectoriser les courriers électroniques en utilisant la transformation TF-IDF
+vectorizer = TfidfVectorizer()
+features_train = vectorizer.fit_transform(emails_train)
+features_test = vectorizer.transform(emails_test)
+
+# Prédiction sur l'ensemble de test
+svmmodelSpam.fit(features_train, labels_train)
+
+mail_pred = svmmodelSpam.predict(features_test)
+# Évaluer les performances du modèle
+accuracy = accuracy_score(labels_test, mail_pred)
+precision = precision_score(labels_test, mail_pred)
+recall = recall_score(labels_test, mail_pred)
+f1 = f1_score(labels_test, mail_pred)
+fpr, tpr, _ = roc_curve(labels_test, mail_pred)
+auc_score = auc(fpr, tpr)
+
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 score:", f1)
+print("AUC score:", auc_score)
+
+
+
---
+++ models/SVMModelSpam.py
@@ -0,0 +1,19 @@
+from sklearn import svm
+import pandas as pd
+
+
+def import_data(file_path):
+    data = pd.read_csv(file_path)
+    # effectuer les opérations de prétraitement nécessaires
+    return data
+
+
+class SVMModelSpam:
+    def __init__(self):
+        self.model = svm.SVC()
+
+    def fit(self, x_train, y_train):
+        self.model.fit(x_train, y_train)
+
+    def predict(self, x_test):
+        return self.model.predict(x_test)
