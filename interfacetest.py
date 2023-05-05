8c1fa1e4e3d0dfb35685e2f74081607d7e28fe82
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-05-05 12:17:06 +0100

    description

--- interfacetest.py
+++ interfacetest.py
@@ -2,13 +2,11 @@
 import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
+from tkinter import ttk
 
 def getValeur() :
     val = testSize.get()
     print(val)
-
-def update_label(*args):
-    selected_value.set(listbox.get(tk.ACTIVE))
 
 
 #creation de splash screen
@@ -38,7 +36,7 @@ splash_label = Label(splash_root, text="Chargement en cours...")
 splash_label.pack()
 
 # Définissez le temps d'affichage du splash screen en millisecondes
-splash_time = 5000
+splash_time = 3000
 
 # Fermez la fenêtre du splash screen après le temps d'affichage
 splash_root.after(splash_time, splash_root.destroy)
@@ -69,39 +67,56 @@ datalabel = tk.Label(f1, text="choisir le dataset : ", bg=bg_color_frame,font=("
 datalabel.pack(padx=50, pady=10)
 
 # Créer une liste déroulante
-listbox = tk.Listbox(f1, width=60)
-listbox.insert(1, "Dataset Spam Email")
-listbox.insert(2, "Dataset Maladies Cardiaques")
-listbox.pack()
+datasets = ["Dataset Spam Email" , "Dataset Maladies Cardiaques" ]
+combo_box = ttk.Combobox(f1, values=datasets,font=("Helvetica", 13), width=35)
+combo_box.pack(padx=50, pady=5)
 
-# Créer une variable de contrôle pour stocker la valeur sélectionnée
-selected_value = tk.StringVar()
+# Configuration des couleurs de fond et de texte pour la ComboBox
+combo_box.configure(background="#F5F5F5", foreground="black" )
 
-# Lier la variable de contrôle à la liste déroulante
-selected_value.trace('w', update_label)
+# Fonction appelée lors de la sélection d'une option dans la ComboBox
+def update_label(*args):
+    selected_value = combo_box.get()
+    if selected_value=="Dataset Spam Email" :
+        descriptiontxt.configure(text="Le fichier csv contient 5172 lignes, chaque ligne"
+                                      " pour chaque e-mail. Il y a 3002 colonnes. La première colonne indique le nom de l'e-mail."
+                                      " Le nom a été défini avec des chiffres et non avec le nom des destinataires pour protéger la confidentialité. "
+                                      "La dernière colonne contient les libellés de prédiction : 1 pour spam, 0 pour non spam."
+                                      " Les 3000 colonnes restantes sont les 3000 mots les plus courants dans tous les e-mails,"
+                                      " après exclusion des caractères/mots non alphabétiques. Pour chaque ligne, "
+                                      "le nombre de chaque mot (colonne) dans cet e-mail (ligne) est stocké dans les cellules respectives. "
+                                       )
+    elif selected_value=="Dataset Maladies Cardiaques" :
+        descriptiontxt.configure(text="description de maladie cardiaque ")
 
-# Créer une étiquette de texte pour afficher la valeur sélectionnée
-label = tk.Label(f1, textvariable=selected_value , bg=bg_color_frame,font=("Helvetica", 14))
-label.pack(padx=50, pady=10)
+description = tk.Label(f1, text="description : ",bg=bg_color_frame, font=("Helvetica", 13))
+description.pack(padx=20, pady=5)
 
-bg_color = "#fdfdfd"
+# Création du Label pour afficher la valeur sélectionnée
+descriptiontxt = tk.Label(f1, text=" ",bg=bg_color_frame , font=("Helvetica", 11) ,wraplength=360 , justify="left")
+descriptiontxt.pack(padx=5, pady=5)
 
-tstsize = tk.Label(f1, text="test size: ", bg=bg_color_frame,font=("Helvetica", 14))
+# Configuration de la ComboBox pour appeler la fonction update_label lors de la sélection d'une option
+combo_box.bind("<<ComboboxSelected>>", update_label)
+
+bg_color = "#fefefe"
+
+tstsize = tk.Label(f1, text="test size: ", bg=bg_color_frame,font=("Helvetica", 13))
 tstsize.pack(padx=50, pady=10)
 
-testSize = tk.Entry(f1,width=40, bg=bg_color, fg="black" ,font=("Helvetica", 14) , bd=0, highlightthickness=1, highlightcolor="gray")
+testSize = tk.Entry(f1,width=40, bg=bg_color, fg="black" ,font=("Helvetica", 13) , bd=0, highlightthickness=1, highlightcolor="gray")
 testSize.pack(padx=40, pady=2 , ipady=5)
 
-parac = tk.Label(f1, text="parametre C: ",bg=bg_color_frame, font=("Helvetica", 14))
+parac = tk.Label(f1, text="parametre C: ",bg=bg_color_frame, font=("Helvetica", 13))
 parac.pack(padx=50, pady=10)
 
-paramC = tk.Entry(f1, width=40, bg=bg_color, fg="black" ,font=("Helvetica", 14) , bd=0, highlightthickness=1, highlightcolor="gray")
+paramC = tk.Entry(f1, width=40, bg=bg_color, fg="black" ,font=("Helvetica", 13) , bd=0, highlightthickness=1, highlightcolor="gray")
 paramC.pack(padx=40, pady=2 , ipady=5)
 
-paramk = tk.Label(f1, text="parametre Kernel: ",bg=bg_color_frame, font=("Helvetica", 14))
+paramk = tk.Label(f1, text="parametre Kernel: ",bg=bg_color_frame, font=("Helvetica", 13))
 paramk.pack(padx=50, pady=10)
 
-paramKernel = tk.Entry(f1, width=40, bg=bg_color, fg="black" ,font=("Helvetica", 14), bd=0, highlightthickness=1, highlightcolor="gray")
+paramKernel = tk.Entry(f1, width=40, bg=bg_color, fg="black" ,font=("Helvetica", 13), bd=0, highlightthickness=1, highlightcolor="gray")
 paramKernel.pack(padx=40, pady=2 , ipady=5)
 
 btn = tk.Button(f1, height=1, width=10, text="Lire", command=getValeur)
