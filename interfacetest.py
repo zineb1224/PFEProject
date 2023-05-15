b6d119b45bdc8e8671e97a2cb0c222ba16d6bccb
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-05-15 15:14:29 +0100

    model maladie

--- interfacetest.py
+++ interfacetest.py
@@ -9,7 +9,7 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, ConfusionMatrixDisplay, f1_score
 from sklearn.model_selection import train_test_split
 
 from models.SVMModelSpam import SVMModelSpam, import_data
@@ -40,6 +40,7 @@ def fitModel() :
         #tracer_grapheSpam_train(kernel, sizetest)
     elif selected_value == "Dataset Maladies Cardiaques":
         trainModelSVMMaladie(kernel, float(sizetest), float(C))
+        tracer_grapheMaladie_train(kernel, float(sizetest), float(C))
 
 #fct pour tester le model et afficher le graphe du test
 def tracerGraphe() :
@@ -52,6 +53,8 @@ def tracerGraphe() :
         #tracer_grapheSpam_test(kernel, float(sizetest))
     elif selected_value == "Dataset Maladies Cardiaques":
         testModelSvmMaladie(kernel, float(sizetest), float(C))
+        tracer_grapheMaladie_test(kernel, float(sizetest), float(C))
+        tracer_matriceConfusionMaladie(kernel, float(sizetest), float(C))
 
 #fct pour entrainer le model du Spam
 def trainModelSvmSpam(kernel, testsize ,c) :
@@ -95,8 +98,8 @@ def trainModelSVMMaladie(kernel, testsize, c):
     # Chargement des données
     maladie_data = import_data("datasets/dataset_maladie.csv")
     # Séparation des données et de target
-    y_maladie = maladie_data['target']
-    X_maladie = maladie_data.drop(['target'], axis=1)
+    X_maladie = maladie_data[["age", "thalach"]].to_numpy()
+    y_maladie = maladie_data["target"].values
 
     # Diviser les données en ensembles d'entraînement et de test
     featuresMaladie_train, featuresMaladie_test, targetMaladie_train, targetMaladie_test = train_test_split(X_maladie, y_maladie, test_size=testsize, random_state=0)
@@ -112,9 +115,119 @@ def testModelSvmMaladie(kernel, testsize, c) :
     # Évaluer les performances du modèle
     accuracy = accuracy_score(targetMaladie_test, mail_pred)
     precision = precision_score(targetMaladie_test, mail_pred)
+    # Calcul du score F1
+    f1 = f1_score(targetMaladie_test, mail_pred)
+    # Affichage du score F1
+    print("Score F1 : ", f1)
     accuracyLabel.configure(text=str(accuracy))
+    scoreLabel.configure(text=str(f1))
     print("Accuracy:", accuracy)
     print("Precision:", precision)
+
+canvas_metrics = None
+def tracer_matriceConfusionMaladie(kernel, testSize, C):
+    global canvas_metrics
+    # Détruire le canvas s'il existe déjà
+    if canvas_metrics:
+        canvas_metrics.get_tk_widget().destroy()
+
+    # Entraîner le modèle SVM et extraire l'objet de modèle SVM
+    model_tuple = trainModelSVMMaladie(kernel, float(testSize), C)
+    svmmodelMaladieCardiaque = model_tuple[2]
+
+    cm = ConfusionMatrixDisplay.from_predictions(model_tuple[4],
+                                                 svmmodelMaladieCardiaque.predict(model_tuple[1]))
+
+    # 3. Obtenir la figure de la matrice de confusion
+    fig, ax = plt.subplots(figsize=(4, 4))
+    cm.plot(ax=ax)
+
+    # 4. Créer un widget Tkinter pour afficher la figure
+    canvas = FigureCanvasTkAgg(fig, master=f_matriceC)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.RIGHT)
+
+# Fonction pour tracer le graphe avec les données d'entraînement du maladie
+canvas_test = None
+def tracer_grapheMaladie_test(kernel, testSize ,C):
+    global canvas_test
+    # Détruire le canvas s'il existe déjà
+    if canvas_test:
+        canvas_test.get_tk_widget().destroy()
+    # Entraîner le modèle SVM et extraire l'objet de modèle SVM
+    model_tuple = trainModelSVMMaladie(kernel, float(testSize), C)
+    svmmodelMaladieCardiaque = model_tuple[2]
+
+    # Création du graphe avec la marge et les vecteurs de support
+    fig = plt.figure(figsize=(4, 4))
+    # afficher les données
+    plt.plot(model_tuple[1][:, 0][model_tuple[4] == 0], model_tuple[1][:, 1][model_tuple[4] == 0],
+             "yo")
+    # afficher les données
+    plt.plot(model_tuple[1][:, 0][model_tuple[4] == 1], model_tuple[1][:, 1][model_tuple[4] == 1],
+             "bo")
+    # Limites du cadre
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    support_vectors_ = svmmodelMaladieCardiaque.support_vectors_()
+    # Marquer les vecteurs de support d'une croix
+    ax.scatter(support_vectors_[:, 0], support_vectors_[:, 1], linewidth=1, facecolors='#FFAAAA', s=180)
+    # Grille de points sur lesquels appliquer le modèle
+    xx = np.linspace(xlim[0], xlim[1], 30)
+    yy = np.linspace(ylim[0], ylim[1], 30)
+    YY, XX = np.meshgrid(yy, xx)
+    xy = np.vstack([XX.ravel(), YY.ravel()]).T
+    # Prédire pour les points de la grille
+    Z = svmmodelMaladieCardiaque.decision_function(xy).reshape(XX.shape)
+    # Afficher la frontière de décision et la marge
+    ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1],
+               alpha=0.5, linestyles=['--', '-', '--'])
+
+# Fonction pour tracer le graphe avec les données d'entraînement du maladie
+canvas_train = None
+def tracer_grapheMaladie_train(kernel, testSize, C):
+    global canvas_train
+    # Détruire le canvas s'il existe déjà
+    if canvas_train:
+        canvas_train.get_tk_widget().destroy()
+    # Entraîner le modèle SVM et extraire l'objet de modèle SVM
+    model_tuple = trainModelSVMMaladie(kernel, float(testSize), C)
+    svmmodelMaladieCardiaque = model_tuple[2]
+    # Création du graphe avec la marge et les vecteurs de support
+    fig = plt.figure(figsize=(4, 4))
+    # afficher les données
+    plt.plot(model_tuple[1][:, 0][model_tuple[4] == 0], model_tuple[1][:, 1][model_tuple[4] == 0],
+                 "yo")
+    # afficher les données
+    plt.plot(model_tuple[1][:, 0][model_tuple[4] == 1], model_tuple[1][:, 1][model_tuple[4] == 1],
+                 "bo")
+    # Limites du cadre
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    support_vectors_ = svmmodelMaladieCardiaque.support_vectors_()
+    # Marquer les vecteurs de support d'une croix
+    ax.scatter(support_vectors_[:, 0], support_vectors_[:, 1], linewidth=1, facecolors='#FFAAAA', s=180)
+    # Grille de points sur lesquels appliquer le modèle
+    xx = np.linspace(xlim[0], xlim[1], 30)
+    yy = np.linspace(ylim[0], ylim[1], 30)
+    YY, XX = np.meshgrid(yy, xx)
+    xy = np.vstack([XX.ravel(), YY.ravel()]).T
+    # Prédire pour les points de la grille
+    Z = svmmodelMaladieCardiaque.decision_function(xy).reshape(XX.shape)
+    # Afficher la frontière de décision et la marge
+    ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1],
+                   alpha=0.5, linestyles=['--', '-', '--'])
+
+    # Tracé du graphe avec les vecteurs de support et les marges
+    plt.scatter(support_vectors_[:, 0], support_vectors_[:, 1], s=100, facecolors='none', edgecolors='k')
+    plt.xlabel('Âge')
+    plt.ylabel('Taux de cholestérol')
+    # Créer le canvas pour afficher le graphe
+    canvas_train = FigureCanvasTkAgg(fig, master=f_graphe)
+    canvas_train.draw()
+    canvas_train.get_tk_widget().pack(side=tk.LEFT)
 
 #fct pour verifier que les inputs sont bien remplis et rendre le boutton de train et de test normal
 def check_fields():
@@ -226,17 +339,22 @@ splash_root = tk.Tk()
 # Centrer la fenêtre au milieu de l'écran
 screen_width = splash_root.winfo_screenwidth()
 screen_height = splash_root.winfo_screenheight()
-x = int((screen_width / 2) - (1300 / 2))
-y = int((screen_height / 2) - (750 / 2))
-splash_root.geometry(f"1300x750+{x}+{y}")
+x = int((screen_width / 2) - (1600 / 2))
+y = int((screen_height / 2) - (950 / 2))
+splash_root.geometry(f"1600x950+{x}+{y}")
 
 # Rendre la fenêtre non-redimensionnable
 splash_root.resizable(width=False, height=False)
 
 # Créez un canevas pour ajouter une image
-splash_canvas = Canvas(splash_root, width=1300, height=750)
+splash_canvas = Canvas(splash_root, width=1600, height=950)
 splash_canvas.pack()
+logo_img=tk.PhotoImage(file="imgs/16x16.png")
+# Définition du logo pour la fenêtre
+splash_root.iconphoto(True, logo_img)
 
+# Affichage de votre logo dans la barre de titre
+splash_root.tk.call('wm', 'iconphoto', splash_root._w,logo_img)
 # Charger l'image et la convertir pour Tkinter
 image = Image.open("imgs/AI.gif")
 photo = ImageTk.PhotoImage(image)
@@ -267,9 +385,9 @@ appSVM.title("Interface Home Machine")
 # Centrer la fenêtre au milieu de l'écran
 screen_width = appSVM.winfo_screenwidth()
 screen_height = appSVM.winfo_screenheight()
-x = int((screen_width / 2) - (1300 / 2))
-y = int((screen_height / 2) - (750 / 2))
-appSVM.geometry(f"1300x750+{x}+{y}")
+x = int((screen_width / 2) - (1600 / 2))
+y = int((screen_height / 2) - (950 / 2))
+appSVM.geometry(f"1600x950+{x}+{y}")
 appSVM.config(bg=BG_COLOR)
 
 # Rendre la fenêtre non-redimensionnable
@@ -277,8 +395,16 @@ appSVM.resizable(width=False, height=False)
 
 f1 = tk.LabelFrame(appSVM, bd=2, text="", bg=bg_color_frame, relief="groove")
 f2 = tk.LabelFrame(appSVM, bd=2, text="", bg=bg_color_frame, relief="groove")
+f3_btn = tk.LabelFrame(f2, bd=2, text="", bg=bg_color_frame)
+f4_grp = tk.LabelFrame(f2, bd=2, text="", bg=bg_color_frame)
+f_matriceC = tk.LabelFrame(f4_grp, bd=2, text="", bg=bg_color_frame)
+f_graphe = tk.LabelFrame(f4_grp, bd=2, text="", bg=bg_color_frame)
 f1.pack(side=tk.LEFT, padx=20, pady=20)
 f2.pack(side=tk.RIGHT, padx=20, pady=20)
+f3_btn.pack(side=tk.TOP)
+f4_grp.pack(side=tk.BOTTOM)
+f_matriceC.pack(side=tk.RIGHT)
+f_graphe.pack(side=tk.LEFT)
 
 datalabel = tk.Label(f1, text="choisir le dataset : ", fg="#d9d9d9", bg=bg_color_frame, font=("Helvetica", 13))
 datalabel.pack(padx=50, pady=10)
@@ -329,19 +455,17 @@ paramKernel.pack(pady=8,ipady=5)
 #icon_training = PhotoImage(file="imgs/training_80px.gif")
 
 #creation de boutton pour entrainer le modele
-btnTraining = tk.Button(f2 , height=4, width=26, text="Training", font=('Helvetica', 15), fg='#FFFFFF', bg='#9AC8EB', bd=0, command=fitModel, state="disabled")
-btnTraining.pack(padx=20,pady=5)
-
-# Création d'un cadre dans la fenêtre Tkinter pour y afficher le graphe
-frame_graphe = tk.LabelFrame(f2, bd=0, bg="#f3f3f3", relief="groove")
-frame_graphe.pack()
+btnTraining = tk.Button(f3_btn , height=4, width=26, text="Training", font=('Helvetica', 15), fg='#FFFFFF', bg='#9AC8EB', bd=0, command=fitModel, state="disabled")
+btnTraining.pack(padx=20,pady=5, side=tk.LEFT)
 
 #creation de boutton pour tester le modele
-btnTesting = tk.Button(f2 , height=4, width=26, text="Testing", font=('Helvetica', 15), fg='#FFFFFF', bg='#9AC8EB', bd=0, command=tracerGraphe, state="disabled")
-btnTesting.pack(padx=20,pady=5)
+btnTesting = tk.Button(f3_btn , height=4, width=26, text="Testing", font=('Helvetica', 15), fg='#FFFFFF', bg='#9AC8EB', bd=0, command=tracerGraphe, state="disabled")
+btnTesting.pack(padx=20,pady=5, side=tk.RIGHT)
 
 accuracyLabel = tk.Label(f2, text="", fg="#d9d9d9", bg=bg_color_frame, font=("Helvetica", 12))
 accuracyLabel.pack(padx=50, pady=10)
+scoreLabel = tk.Label(f2, text="", fg="#d9d9d9", bg=bg_color_frame, font=("Helvetica", 12))
+scoreLabel.pack(padx=50, pady=10)
 
 testSize.bind("<KeyRelease>", lambda event: check_fields())
 paramC.bind("<KeyRelease>", lambda event: check_fields())
--- penguintest.py
+++ penguintest.py
@@ -5,8 +5,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn import metrics
+
+from models.SVMModelPenguin import SVMModelPenguin , import_data
+
 # Chargement des données
-penguins = pd.read_csv("datasets/penguins.csv")
+svmmodelPenguin = SVMModelPenguin()
+penguins = import_data("datasets/penguins.csv")
 data = penguins[["bill_length_mm",
  "bill_depth_mm",
  "flipper_length_mm",
@@ -22,62 +26,51 @@ X = data[:,:2][Adelie_or_Gentoo]
 y = y[Adelie_or_Gentoo]
 print(X.shape)
 print(y.shape)
-# afficher les données (pingouins Adelie et Gentoo )
-plt.figure(figsize=(6, 4))
-plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", label="Adelie")
-plt.plot(X[:, 0][y==2], X[:, 1][y==2], "bo", label="Gentoo")
-# Légende
-plt.legend()
-plt.xlabel("Bill length (mm)")
-plt.ylabel("Bill depth (mm)")
-plt.title("Données")
-plt.tight_layout()
-plt.show()
 
 from sklearn.impute import SimpleImputer
 
 # Instantiate the imputer
 imputer = SimpleImputer(strategy='mean') # or 'median', 'most_frequent' etc.
-
-# Replace missing values in X with the mean of each column
+# Impute missing values in X
 X = imputer.fit_transform(X)
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # initialisation
-for C1 in 100, 0.1:
- model_svc1 = svm.SVC(kernel='linear', C=C1)
- # entrainement
- model_svc1.fit(X, y)
- #Evaluation
- score1 = model_svc1.score(X, y)
- print("Score de la SVM linéaire (C=%.2f) (mean accuracy): %.2f"%( C1, score1) )
- metrics.ConfusionMatrixDisplay.from_predictions(y, model_svc1.predict(X))
- # afficher les données (pingouins Adelie et Gentoo)
- plt.figure(figsize=(6, 4))
- plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", label="Adelie")
- plt.plot(X[:, 0][y==2], X[:, 1][y==2], "bo", label="Gentoo")
- # Limites du cadre
- ax = plt.gca()
- xlim = ax.get_xlim()
- ylim = ax.get_ylim()
- # Marquer les points vecteurs de support
- ax.scatter(model_svc1.support_vectors_[:, 0],
- model_svc1.support_vectors_[:, 1],
- linewidth=1,
- facecolors='#FFAAAA', s=180)
- # Grille de points sur lesquels appliquer le modèle
- xx = np.linspace(xlim[0], xlim[1], 30)
- yy = np.linspace(ylim[0], ylim[1], 30)
- YY, XX = np.meshgrid(yy, xx)
- xy = np.vstack([XX.ravel(), YY.ravel()]).T
- # Prédire pour les points de la grille
- Z = model_svc1.decision_function(xy).reshape(XX.shape)
- # Afficher la frontière de décision et la marge
- ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1],
- alpha=0.5, linestyles=['--', '-', '--'])
- # Légende
- plt.legend()
- plt.xlabel("Bill length (mm)")
- plt.ylabel("Bill depth (mm)")
- plt.title("SVM linéaire (C=%.2f)"%C1)
- plt.tight_layout()
- plt.show()
+C1 = 0.1
+# entrainement
+svmmodelPenguin.fit(X_train, y_train)
+
+# Evaluation
+metrics.ConfusionMatrixDisplay.from_predictions(y_test, svmmodelPenguin.predict(X_test))
+
+# afficher les données (pingouins Adelie et Gentoo)
+plt.figure(figsize=(6, 4))
+plt.plot(X_train[:, 0][y_train==0], X_train[:, 1][y_train==0], "yo", label="Adelie")
+plt.plot(X_train[:, 0][y_train==2], X_train[:, 1][y_train==2], "bo", label="Gentoo")
+# Limites du cadre
+ax = plt.gca()
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+# Récupération des vecteurs de support
+support_vectors = svmmodelPenguin.support_vectors_()
+
+# Marquer les points vecteurs de support
+ax.scatter(support_vectors[:, 0],support_vectors[:, 1],linewidth=1,facecolors='#FFAAAA', s=180)
+# Grille de points sur lesquels appliquer le modèle
+xx = np.linspace(xlim[0], xlim[1], 30)
+yy = np.linspace(ylim[0], ylim[1], 30)
+YY, XX = np.meshgrid(yy, xx)
+xy = np.vstack([XX.ravel(), YY.ravel()]).T
+# Prédire pour les points de la grille
+Z = svmmodelPenguin.decision_function(xy).reshape(XX.shape)
+# Afficher la frontière de décision et la marge
+ax.contour(XX, YY, Z, colors='k', levels=[-1, 0, 1],
+alpha=0.5, linestyles=['--', '-', '--'])
+# Légende
+plt.legend()
+plt.xlabel("Bill length (mm)")
+plt.ylabel("Bill depth (mm)")
+plt.title("SVM linéaire (C=%.2f)"%C1)
+plt.tight_layout()
+plt.show()
