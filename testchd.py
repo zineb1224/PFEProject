af1304e8afee273066da45942c257170d0e75e38
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-05-19 23:30:40 +0100

    update

--- testchd.py
+++ testchd.py
@@ -1,11 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 def afficher_description():
     # Sélectionner l'onglet 2
     notebook.select(onglet2)
-
 
 # Création de la fenêtre principale
 fenetre_principale = tk.Tk()
@@ -23,8 +21,7 @@ onglet1 = ttk.Frame(notebook)
 notebook.add(onglet1, text='Onglet 1')
 
 # Création du bouton dans l'onglet 1
-bouton_onglet1 = ttk.Button(onglet1, text="Voir plus de description", command=afficher_description,
-                            style="Custom.TButton")
+bouton_onglet1 = ttk.Button(onglet1, text="Voir plus de description", command=afficher_description, style="Custom.TButton")
 bouton_onglet1.pack(padx=20, pady=20)
 
 # Création du deuxième onglet
@@ -33,6 +30,9 @@ notebook.add(onglet2, text='Onglet 2')
 
 # L'onglet 2 est initialisé masqué
 notebook.hide(onglet2)
+
+# Affichage du Notebook
+notebook.pack(expand=True, fill="both")
 
 # Lancement de la boucle principale
 fenetre_principale.mainloop()
