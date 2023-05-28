fdb3ec7d444f6ce90d89cab85a9dfc1ce69d85c9
ZinebElhouz <zineb.elhouz@usmba.ac.ma>
2023-05-28 01:41:45 +0100

    update

--- bf.py
+++ bf.py
@@ -16,27 +16,37 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = SVC(kernel='rbf')
 model.fit(X_train, y_train)
 
-# Extraction des vecteurs de support
-support_vectors = model.support_vectors_
+# Extraction des indices des vecteurs de support
+support_indices = model.support_
+dual_coef = np.abs(model.dual_coef_)
 
-# Obtention des poids et du biais
-dual_coef = model.dual_coef_
-intercept = model.intercept_
+# Limiter le nombre de vecteurs de support à afficher
+num_support_vectors = 3  # Nombre souhaité de vecteurs de support à afficher par classe
+selected_support_vectors = []
 
-# Plot des données de test et des vecteurs de support
+for class_label in np.unique(y_train):
+    class_dual_coef = dual_coef[class_label - 1]
+    class_support_indices = support_indices[np.where(y_train[support_indices] == class_label)]
+    num_vectors = min(num_support_vectors, len(class_support_indices))
+    random_indices = np.random.choice(class_support_indices, num_vectors, replace=False)
+    selected_support_vectors.extend(X_train[random_indices])
+
+selected_support_vectors = np.array(selected_support_vectors)
+
+# Plot des données de test et des vecteurs de support sélectionnés
 plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap='viridis')
-plt.scatter(support_vectors[:, 0], support_vectors[:, 1], s=70, facecolors='none', edgecolors='k')
+plt.scatter(selected_support_vectors[:, 0], selected_support_vectors[:, 1], s=100, facecolors='none', edgecolors='k')
 
 # Calcul des coordonnées de l'hyperplan
-x_min, x_max = X_test[:, 0].min() - 1, X_test[:, 0].max() + 1
-y_min, y_max = X_test[:, 1].min() - 1, X_test[:, 1].max() + 1
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02), np.arange(y_min, y_max, 0.02))
 Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 # Plot de l'hyperplan et des marges
 plt.contourf(xx, yy, Z, alpha=0.8, cmap='viridis')
-plt.scatter(support_vectors[:, 0], support_vectors[:, 1], s=70, facecolors='none', edgecolors='k')
+plt.scatter(selected_support_vectors[:, 0], selected_support_vectors[:, 1], s=100, facecolors='none', edgecolors='k')
 plt.xlabel('Caractéristique 1')
 plt.ylabel('Caractéristique 2')
 plt.title('Hyperplan et marges du modèle SVM')
